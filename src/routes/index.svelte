709aa48db4772d9f5477aecdf25948832acc0610
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-07-24 12:21:18 +0530

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+  let QrCode;
+  onMount(async () => {
+    QrCode = (await import("https://cdn.jsdelivr.net/npm/svelte-qrcode"))
+      .default;
+  });
+</script>
+
+{#await QrCode}
+  <p>...waiting</p>
+{:then component}
+  <svelte:component this={component} value="123" />
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
